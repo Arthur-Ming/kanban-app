3e6c2e09e80c09ab3f17b6d07733f407b4fddd52
Artur Mingazhov <arthurming7@gmail.com>
2022-12-25 09:37:39 +0300

    refactor: styles variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { LanguageProvider } from './contexts/language-context';
 import App from './App';
 import store from './redux/store';
-import 'normalize.css';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
