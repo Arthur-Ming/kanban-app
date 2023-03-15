a94e3c263870da67643895e3d6baae0b3b76ce31
Artur Mingazhov <arthurming7@gmail.com>
2023-03-15 22:30:16 +0300

    refactor: auth styles

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,40 @@
+import { IFetchError } from 'interfaces';
+import { withErrorBoundary } from 'react-error-boundary';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { useLoginUserMutation } from 'redux/api/users';
 import LoginForm from './LoginForm';
+import styles from './index.module.scss';
 
 const Login = () => {
-  const [loginUser, { isLoading, isSuccess }] = useLoginUserMutation();
+  const [loginUser, { isLoading, isSuccess, isError, error }] = useLoginUserMutation();
+
   const navigate = useNavigate();
 
+  if (isError) throw error;
   if (isSuccess) {
     if (window.history.state && window.history.state.idx > 0) {
       navigate(-1);
     }
   }
-  return <LoginForm onSubmit={loginUser} isLoading={isLoading} />;
+  return (
+    <main className={styles.root}>
+      <LoginForm onSubmit={loginUser} isLoading={isLoading} />
+    </main>
+  );
 };
 
-export default Login;
+export default withErrorBoundary(Login, {
+  fallbackRender: ({ error, resetErrorBoundary }) => {
+    const errorStatus = (error as unknown as IFetchError)?.status;
+
+    if (errorStatus === 404 || errorStatus === 403) {
+      toast.error('неверные учетные данные!', {
+        toastId: errorStatus,
+      });
+    }
+    return <Login />;
+  },
+});
+/* 
+export default Login; */
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,15 @@
 import { useRegisterUserMutation } from 'redux/api/users';
 import RegisterForm from './RegisterForm';
+import styles from './index.module.scss';
 
 const Register = () => {
   const [addUser, { isLoading }] = useRegisterUserMutation();
 
-  return <RegisterForm onSubmit={addUser} isLoading={isLoading} />;
+  return (
+    <main className={styles.root}>
+      <RegisterForm onSubmit={addUser} isLoading={isLoading} />
+    </main>
+  );
 };
 
 export default Register;
